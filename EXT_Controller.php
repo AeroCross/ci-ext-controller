bd39517b891468caf0fe11dedeb475830d69d6f9
Mario Cuba <mario@mariocuba.net>
2012-09-01 11:27:59 -0430

    Lots of changes. The commit history is located at the gist (@see https://gist.github.com/2355901)
    Changes include:
    
    - Comments
    - New names and additions to public variables
    - Folders and subfolders calculation
    - Calculation of layout to include

--- EXT_Controller.php
+++ EXT_Controller.php
@@ -15,7 +15,7 @@
 *
 * This source file is subject to the Creative Commons Attribution 3.0 Unported License
 * that is available through http://creativecommons.org/licenses/by/3.0/legalcode. It is
-* released to the public domain via http://github.com/AeroCross/ci-core-extensions.
+* released to the public domain via http://github.com/AeroCross/ci-ext-controller.
 *
 * You are free to share, modify, and profit from this source file as long as
 * there is attribution to the author and this Notice of License is not removed.
@@ -30,11 +30,12 @@ class EXT_Controller extends CI_Controller {
 	// the data to be passed around views and controllers
 	public $data;
 	public $view	= TRUE;
-	public $layout	= TRUE;
+	public $content	= TRUE;
+	public $layout	= NULL;
 
 	public function __construct() {
 		parent::__construct();
-		
+
 		// required to suppress strict errors
 		$this->data	= new StdClass;
 
@@ -50,6 +51,7 @@ class EXT_Controller extends CI_Controller {
 	* @param	string	- the methods and variables
 	*/
 	public function _remap($method, $parameters) {
+
 		if (method_exists($this, $method)) {
 
 			// $this 	- the controller name (as in the class definition)
@@ -57,17 +59,27 @@ class EXT_Controller extends CI_Controller {
 
 			call_user_func_array(array($this, $method), $parameters);
 
-			// set the path to the view files
-			$view = strtolower('files/' . get_class($this)) . '/' . $method;
-
 			// does a specific view needs to be loaded?
 			if (is_string($this->view) && !empty($this->view)) {
 				$view = $this->view;
+
+			// it doesn't - calculate the folder
+			} else {
+				// set the path to the view files
+				$view		= strtolower('files/' . get_class($this));	
+
+				// check if there's a subfolder in views
+				if ($this->uri->segment(1) !== NULL AND $this->uri->segment(1) !== strtolower(get_class($this))) {
+					$view	= strtolower('files/' . $this->uri->segment(1) . '/' . get_class($this));
+				}
+
+				// complete the view
+				$view .= '/' . $method;
 			}
 
 			// autoload the view
 			if ($this->view !== FALSE) {
-				$this->data->yield = $this->load->view($view, $this->data, TRUE);
+				$this->data->content = $this->load->view($view, $this->data, TRUE);
 
 				// check if there's a custom layout
 				if (is_string($this->layout) AND !empty($this->layout)) {
@@ -75,20 +87,20 @@ class EXT_Controller extends CI_Controller {
 
 				// set the path to check for an existing layout
 				} elseif (file_exists(APPPATH . 'views/layouts/' . strtolower(get_class($this) . '.php'))) {
-					$layout = 'layouts/' . strtolower(get_class($this));
+					$layout = strtolower(get_class($this));
 
 				// if all else fails, set the main layout
 				} else {
-					$layout = 'layouts/default';
+					$layout = 'default';
 				}
 
 				// if the layout is going to be called
-				if ($this->layout === TRUE) {
-					$this->load->view($layout, $this->data);
+				if (is_string($layout) AND !empty($layout)) {
+					$this->load->view('layouts/' . $layout, $this->data);
 
 				// if not, echo directly to output for ajax calls, file handling, etc.
 				} else {
-					echo $this->data->yield;
+					echo $this->data->content;
 				}
 			}
 
--- README.md
+++
@@ -1,4 +0,0 @@
-ci-ext-controller
-=================
-
-Codeigniter extended controller for autoloading views and global initialization of processes.
